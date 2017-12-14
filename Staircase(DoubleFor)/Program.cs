41a9cffa95a4bbaadcc689bd2c7caba866aa84cf
Станислав Шульгович <Stas_shulgovich@mail.ru>
2017-12-15 01:04:59 +0300

    done

--- Staircase(DoubleFor)/Program.cs
+++ Staircase(DoubleFor)/Program.cs
@@ -10,6 +10,24 @@ namespace Staircase_DoubleFor_
     {
         static void Main(string[] args)
         {
+            int n = Convert.ToInt32(Console.ReadLine());
+            string s = $"#";
+            string sps = $" ";
+
+            for (int i = 1; i <= n; i++)
+            {
+                for (int j = n; j > i; j--)
+                {
+                    Console.Write(sps);
+                }
+
+                for (int k = 0; k < i; k++)
+                {
+                    Console.Write(s);
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
         }
     }
 }
